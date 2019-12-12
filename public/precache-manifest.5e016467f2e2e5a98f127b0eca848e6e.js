b8fb3f44c978491c3cb5eafe40294c3c138152bf
Amy Tang <amy@pathfactory.com>
2019-12-11 21:03:25 -0500

    build

---
+++ public/precache-manifest.5e016467f2e2e5a98f127b0eca848e6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c326ae4656d1ae7e75a26494255ea177",
+    "url": "/HackerYou/con-ed-full-stack/index.html"
+  },
+  {
+    "revision": "d510d3e32bce1a73266e",
+    "url": "/HackerYou/con-ed-full-stack/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "06044638e6576f47a0db",
+    "url": "/HackerYou/con-ed-full-stack/static/js/2.60b0a5c2.chunk.js"
+  },
+  {
+    "revision": "a53f20519ac64f9ef1167ee7f61c04b9",
+    "url": "/HackerYou/con-ed-full-stack/static/js/2.60b0a5c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d510d3e32bce1a73266e",
+    "url": "/HackerYou/con-ed-full-stack/static/js/main.4e5d36e9.chunk.js"
+  },
+  {
+    "revision": "ce8534c83b08a0e0835c",
+    "url": "/HackerYou/con-ed-full-stack/static/js/runtime-main.ccc6ac46.js"
+  }
+]);
